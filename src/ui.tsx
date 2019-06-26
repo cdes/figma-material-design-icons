79d8e05b08a6a7a5b5d4ab12dcf397cbbd3d79b0
Ahmad Al Haddad <haddad@ah.sa>
2019-06-26 11:10:11 +0300

    WIP

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,25 +1,98 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import FuzzySearch from "fuzzy-search";
+import { FixedSizeList as List } from 'react-window';
 import './ui.scss'
 
 declare function require(path: string): any
 
+const Column = ({ index, style }) => (
+  <div style={style}>Column {index}</div>
+);
+
 class App extends React.Component {
   textbox: HTMLInputElement
+  version: string
+  iconsData: JSON
+  searcher: FuzzySearch
 
-  countRef = (element: HTMLInputElement) => {
-    if (element) element.value = '5'
-    this.textbox = element
+  componentDidMount = async () => {
+    const packageResponse = await fetch(
+      "https://data.jsdelivr.com/v1/package/npm/@mdi/svg"
+    );
+    const packageJson = await packageResponse.json();
+
+    const latestVersion = packageJson.tags.latest;
+    const storedVersion = figma.root.getPluginData("mdisvgversion");
+    const storedData = figma.root.getPluginData("mdisvgmeta");
+
+    if (
+      latestVersion !== storedVersion ||
+      storedData === null ||
+      storedData.length < 100
+    ) {
+      const mdiResponse = await fetch(
+        `https://cdn.jsdelivr.net/npm/@mdi/svg@${latestVersion}/meta.json`
+      );
+      const mdiJson = await mdiResponse.json();
+
+      const json = mdiJson.map(({ name, tag, aliases }) => ({
+        name,
+        tag,
+        aliases
+      }));
+      const mdiText = JSON.stringify(json);
+
+      window.parent.postMessage({pluginMessage: request.response}, '*')
+      figma.root.setPluginData("mdisvgmeta", mdiText);
+      figma.root.setPluginData("mdisvgversion", latestVersion);
+      this.version = latestVersion;
+
+      this.iconsData = json;
+    } else {
+      this.version = storedVersion;
+      const mdiText = figma.root.getPluginData("mdisvgmeta");
+      const json = JSON.parse(mdiText);
+      this.iconsData = json;
+    }
+
+    this.searcher = new FuzzySearch(
+      this.iconsData,
+      ["name", "aliases", "tags"],
+      {
+        caseSensitive: false
+      }
+    );
   }
 
-  onCreate = () => {
-    const count = parseInt(this.textbox.value, 10)
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
-  }
+  getRows = rows => {
+    const groupSize = 5;
 
-  onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
-  }
+    const iconsRows = rows
+      .map((icon, index) => {
+        return index % groupSize === 0
+          ? rows.slice(index, index + groupSize)
+          : null;
+      })
+      .filter(icon => icon)
+      .map(group => {
+        return group
+          .map(icon => (
+            <div className="mdip-icon" id={`${icon.name}-div`}>
+              <img
+                data-icon-name={`${icon.name}`}
+                src={`https://cdn.jsdelivr.net/npm/@mdi/svg@${
+                  this.version
+                }/svg/${icon.name}.svg`}
+                width={24}
+                height={24}
+              />
+            </div>
+          ));
+      });
+
+    return iconsRows;
+  };
 
   render() {
     return (
@@ -36,10 +109,22 @@ class App extends React.Component {
         </div>
         <div className="scrollable" id="scrollable">
           <div id="mdi-icons">
-            <div className="empty">
-              <h2>Loading Icons...</h2>
-              <p>Sit tight ;)</p>
-            </div>
+            {this.iconsData ?
+              <List
+                height={92}
+                itemCount={1000}
+                itemSize={100}
+                layout="horizontal"
+                width={460}
+              >
+                {Column}
+              </List>
+            :
+              <div className="empty">
+                <h2>Loading Icons...</h2>
+                <p>Sit tight ;)</p>
+              </div>
+            }
           </div>
         </div>
       </div>
