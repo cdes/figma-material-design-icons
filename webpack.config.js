64f878677886830bfdc53d79a3b4557ddb18e36c
Ahmad Alhaddad <haddad@ah.sa>
2023-09-03 10:43:51 +0300

    update allowedDomains

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = (env, argv) => ({
   // This is necessary because Figma's 'eval' works differently than normal eval
@@ -15,18 +14,22 @@ module.exports = (env, argv) => ({
     path: path.resolve(__dirname, 'build'),
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        loader: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader'
-        }],
+        loader: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
       },
     ],
   },
@@ -41,6 +44,5 @@ module.exports = (env, argv) => ({
       chunks: ['ui'],
     }),
     new HtmlWebpackInlineSourcePlugin(),
-    new BundleAnalyzerPlugin()
   ],
-})
+});
